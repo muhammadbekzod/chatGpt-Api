186be3641151dc4484eda233536e8d48f55d2c7d
Istatillo Jo'rayev <istatillo@Istatillos-MacBook-Pro.local>
2024-12-24 16:53:41 +0500

    cors

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 dotenv.config({ path: ".env" });
 const app = express();
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 
 const validateMessages = (messages) => {
